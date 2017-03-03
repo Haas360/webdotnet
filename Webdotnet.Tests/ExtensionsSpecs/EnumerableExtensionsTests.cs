b44eeda76403c307509a01007d7e8107a02fc8b7
Kamil Hadas <hadas.kamil@gmail.com>
2017-03-03 15:48:26 +0100

    Correcting names of methods

--- Webdotnet.Tests/ExtensionsSpecs/EnumerableExtensionsTests.cs
+++ Webdotnet.Tests/ExtensionsSpecs/EnumerableExtensionsTests.cs
@@ -9,19 +9,19 @@ namespace Webdotnet.Tests.ExtensionsSpecs
     public class EnumerableIsNullOrEmptyExtensionTests
     {
         [Test]
-        public void ShouldReturnFalseIfListIsEmpty()
+        public void ShouldReturnTrueIfListIsEmpty()
         {
             var emptyList = new List<string>();
             Assert.IsTrue(emptyList.IsNullOrEmpty());
         }
         [Test]
-        public void ShouldReturnFalseIfNull()
+        public void ShouldReturnTrueIfNull()
         {
             List<string> emptyList = null;
             Assert.IsTrue(emptyList.IsNullOrEmpty());
         }
         [Test]
-        public void ShouldReturnTrueIfListContainsSomething()
+        public void ShouldReturnFalseIfListContainsSomething()
         {
             var list = new List<int>() {1,2};
             Assert.IsFalse(list.IsNullOrEmpty());
@@ -31,19 +31,19 @@ namespace Webdotnet.Tests.ExtensionsSpecs
     public class EnumerableIsNotNullOrEmptyExtensionTests
     {
         [Test]
-        public void ShouldReturnTrueIfListIsEmpty()
+        public void ShouldReturnFalseIfListIsEmpty()
         {
             var emptyList = new List<string>();
             Assert.IsFalse(emptyList.IsNotNullOrEmpty());
         }
         [Test]
-        public void ShouldReturnTrueIfNull()
+        public void ShouldReturnFalseIfNull()
         {
             List<string> emptyList = null;
             Assert.IsFalse(emptyList.IsNotNullOrEmpty());
         }
         [Test]
-        public void ShouldReturnFalseIfListContainsSomething()
+        public void ShouldReturnTrueIfListContainsSomething()
         {
             var list = new List<int>() { 1, 2 };
             Assert.IsTrue(list.IsNotNullOrEmpty());
